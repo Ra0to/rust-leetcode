712d27faf89c888c16b41ee0c60cd074f0b120d6
ra0to <mikl181@yandex.ru>
2023-07-03 23:29:24 +0300

    fix: more elegant solution for #55

--- src/_55_jump_game.rs
+++ src/_55_jump_game.rs
@@ -1,39 +1,16 @@
 struct Solution;
 
-fn fill_reachable(
-    pos: usize,
-    nums: &Vec<i32>,
-    is_reachable: &mut Vec<bool>,
-    is_visited: &mut Vec<bool>,
-) {
-    if is_visited[pos] {
-        return;
-    }
-
-    is_visited[pos] = true;
-
-    let max_pos = pos + nums[pos] as usize;
-
-    if max_pos >= nums.len() - 1 {
-        is_reachable[nums.len() - 1] = true;
-        return;
-    }
-
-    for new_pos in (pos + 1..=max_pos).rev() {
-        is_reachable[new_pos] = true;
-        fill_reachable(new_pos, nums, is_reachable, is_visited);
-    }
-}
-
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        let mut is_reachable = vec![false; nums.len()];
-        let mut is_visited = vec![false; nums.len()];
-        is_reachable[0] = true;
+        let mut max_pos = 0;
+        let mut pos = 0;
 
-        fill_reachable(0, &nums, &mut is_reachable, &mut is_visited);
+        while pos < nums.len() && pos <= max_pos {
+            max_pos = max_pos.max(pos + nums[pos] as usize);
+            pos += 1;
+        }
 
-        is_reachable[nums.len() - 1]
+        max_pos >= nums.len() - 1
     }
 }
 
